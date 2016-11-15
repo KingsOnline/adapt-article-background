6ddb9fadc9ca5355056eb208d6eb98b26f9ab3a6
Louise <louise.bennett@kcl.ac.uk>
2016-11-15 17:12:58 +0000

    Update adapt-article-background.js

--- js/adapt-article-background.js
+++ js/adapt-article-background.js
@@ -67,9 +67,9 @@ define([
 		},
         setBackgroundGraphic: function($articleElement, options) {
             if (Adapt.device.screenSize === 'large') {
-                $articleElement.addClass('article-background-article').remove('article-background-article-mobile').css({'background-image': 'url('+options.src+')', 'background-color': options.backgroundColor + ' !important', 'background-repeat': options.backgroundRepeat, 'background-size': options.backgroundSize, 'background-position':options.backgroundPosition, 'min-height' : options.bannerHeight + 'px'});
+                $articleElement.addClass('article-background-article').remove('article-background-article-mobile').css({'background-image': 'url('+options.src+')', 'background-color': options.backgroundColor + ' !important', 'background-attachment': 'fixed', 'background-repeat': options.backgroundRepeat, 'background-size': options.backgroundSize, 'background-position':options.backgroundPosition, 'min-height' : options.bannerHeight + 'px'});
             } else {
-                $articleElement.addClass('article-background-article-mobile').remove('article-background-article').css({'background-image': 'url('+options.mobileSrc+')', 'background-color': options.backgroundColor + ' !important', 'background-repeat': options.backgroundRepeat, 'background-size': options.backgroundSize, 'background-position':options.backgroundPosition, 'min-height' : options.mobileBannerHeight + 'px'});
+                $articleElement.addClass('article-background-article-mobile').remove('article-background-article').css({'background-image': 'url('+options.mobileSrc+')', 'background-color': options.backgroundColor + ' !important', 'background-attachment': 'fixed', 'background-repeat': options.backgroundRepeat, 'background-size': options.backgroundSize, 'background-position':options.backgroundPosition, 'min-height' : options.mobileBannerHeight + 'px'});
             }
         }
 
